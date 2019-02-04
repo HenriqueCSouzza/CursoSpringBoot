8a06bd36993cc2ea189f79e7c7409198241af687
Henrique C. Souza <henrique.inmetrics@gmail.com>
2019-02-04 15:57:01 -0200

    criando novos dominios Estado e Cidade

--- src/main/java/com/henrique/cursomc/domain/Estado.java
+++ src/main/java/com/henrique/cursomc/domain/Estado.java
@@ -4,57 +4,37 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+public class Estado implements Serializable{
 
-@Entity
-public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	@OneToMany(name="")
+	private List <Cidade> cidades = new ArrayList<Cidade>();
 	
-	@JsonManagedReference
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-
-	public Categoria() {}
-
-	public Categoria(Integer id, String nome) {
+	
+	public Estado() {}
+	
+	public Estado(Integer id,String nome) {
 		super();
-		this.id = id;
-		this.nome = nome;
+		this.id=id;
+		this.nome=nome;
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNome() {
-		return this.nome;
+		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
 	}
 
 	@Override
@@ -73,7 +53,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Estado other = (Estado) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -81,5 +61,7 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-
+	
+	
+	
 }
